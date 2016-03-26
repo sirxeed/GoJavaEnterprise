903f04757f1f6cf34fe8efd669f763c310eb22fe
Andrii Stasiuk <sir.xeed@gmail.com>
2016-03-26 22:30:25 +0200

    initial commit

--- Module03-Multithreading/src/Starter.java
+++ Module03-Multithreading/src/Starter.java
@@ -1,10 +1,41 @@
 public class Starter {
+    private static volatile int counter = 0;
 
     public static void main(String[] args) {
-        SemaphoreImplementation semaphore = new SemaphoreImplementation(2);
+        final SemaphoreImplementation semaphore = new SemaphoreImplementation(2);
         for (int i = 1; i <= 10; i++) {
             Thread thread = new Thread(new Worker(i, semaphore));
             thread.start();
+        }
+    }
+
+    public static class Worker implements Runnable {
+        private int threadIndex;
+        SemaphoreInterface semaphore;
+
+
+        public Worker(int threadIndex, SemaphoreInterface semaphore) {
+            this.threadIndex = threadIndex;
+            this.semaphore = semaphore;
+        }
+
+        @Override
+        public void run() {
+            try {
+                System.out.println("Start of thread #" + threadIndex);
+
+                //потік перевірив дозвіл семафору і якщо він є, то забрав його. Якщо ні - чекає.
+                semaphore.acquire();
+
+                //початок критичної секції
+                counter++;
+                System.out.println("Critical section. Thread #" + threadIndex + ". Counter = " + counter);
+                semaphore.release();//критична секція закінчилась - віддаємо дозвіл.
+
+                System.out.println("End of thread #" + threadIndex);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
