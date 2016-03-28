b31912b01dfa32550d92c476b4f9b63de665550a
Andrii Stasiuk <sir.xeed@gmail.com>
2016-03-28 23:23:48 +0300

    initial commit

--- Module03-Multithreading/src/LectureSynchronizers.java
+++ Module03-Multithreading/src/LectureSynchronizers.java
@@ -1,11 +1,15 @@
+import java.util.Random;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Exchanger;
+import java.util.stream.IntStream;
 
 public class LectureSynchronizers {
 
     public static void main(String[] args) throws InterruptedException {
         LectureSynchronizers synchronizers = new LectureSynchronizers();
-        synchronizers.testCyclicBarrier();
+        //synchronizers.testCyclicBarrier();
+        synchronizers.testExchanger();
     }
 
     public void testCyclicBarrier() throws InterruptedException {
@@ -25,6 +29,21 @@ public class LectureSynchronizers {
             }).start();
             Thread.sleep(1000);
         }
+    }
+
+    public void testExchanger() throws InterruptedException {
+        Random random = new Random();
+        Exchanger<String> exchanger = new Exchanger<>();
+        IntStream.range(0, 2).forEach((i) -> new Thread(() -> {
+            try {
+                Thread.sleep(random.nextInt(1000));
+                System.out.println(Thread.currentThread().getName() + " is ready to exchange");
+                System.out.println(Thread.currentThread().getName() + " < - > " + exchanger.exchange(Thread.currentThread().getName()));
+                exchanger.exchange(Thread.currentThread().getName());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start());
     }
 
 }
